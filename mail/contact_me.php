99ab6c426de272635f805c90e2b3a620b0d613c6
Taha <tkan4045@uni.sydney.edu.au>
2018-11-17 06:07:22 +1100

    PLEASE

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,16 +1,12 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
-}
+
 
 if (isset($_POST['sendMessageButton'])) {
 $name = strip_tags(htmlspecialchars($_POST['name'];
 $email = strip_tags(htmlspecialchars($_POST['email'];
 $phone = strip_tags(htmlspecialchars($_POST['phone'];
 $message = strip_tags(htmlspecialchars($_POST['message'];
-}
+
 
 // Create the email and send the message
 $to = "contact@tahakanj.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
@@ -19,6 +15,8 @@ $body = "You have received a new message.\n\n"."Here are the details:\n\nName: $
 $headers = "From: $name\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email";
 
-if(!mail($to, $subject, $body, $headers))
-  http_response_code(500);
+mail($to, $subject, $headers);
+
+}
+
 ?>
