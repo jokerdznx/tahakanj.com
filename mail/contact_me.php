d1aafa00878e9f7f050f67cf0e15cb5b30640726
Taha <tkan4045@uni.sydney.edu.au>
2018-11-17 05:59:03 +1100

    contact form
    probably last try before i go to bed

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -5,18 +5,20 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || 
   exit();
 }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+if (isset($_POST['endMessageButton'])) {
+$name = strip_tags(htmlspecialchars($_POST['name'];
+$email = strip_tags(htmlspecialchars($_POST['email'];
+$phone = strip_tags(htmlspecialchars($_POST['phone'];
+$message = strip_tags(htmlspecialchars($_POST['message'];
+}
 
 // Create the email and send the message
 $to = "contact@tahakanj.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@tahakanj.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";
+$subject = "Website Contact:  $name";
+$body = "You have received a new message.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: $name\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email";
 
-if(!mail($to, $subject, $body, $header))
+if(!mail($to, $subject, $body, $headers))
   http_response_code(500);
 ?>
